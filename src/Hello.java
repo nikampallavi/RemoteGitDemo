9866ed2a07a6dc2cb4b767bdbcc213b19694854e
nikampallavi <gorep19995@gmail.com>
2019-03-23 13:20:04 +0530

    dev commit

--- src/Hello.java
+++ src/Hello.java
@@ -4,6 +4,7 @@ public class Hello {
 	public static void main(String[] args) {
 		System.out.println("Hello git.....");
 		System.out.println("second commit");
+		System.out.println("chnages by 2nd developer");
 	}
 
 }
